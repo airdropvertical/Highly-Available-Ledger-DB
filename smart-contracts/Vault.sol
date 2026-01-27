daa6d9129f56bf2066ea6e21f11c5e0499dc11a4
airdropvertical <265957748+airdropvertical@users.noreply.github.com>
2026-01-27 10:56:45 +0700

    refactor: optimize memory allocation in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -50,3 +50,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 7270
 // Optimized logic batch 9122
 // Optimized logic batch 2565
+// Optimized logic batch 5832
